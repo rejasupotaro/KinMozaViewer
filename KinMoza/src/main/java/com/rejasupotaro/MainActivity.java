1ab2539a4d23eaf36d815a472edff9c9a8d845b5
Kentaro Takiguchi <takiguchi0817@gmail.com>
2013-09-25 21:00:11 +0900

    Remove ActionBar

--- KinMoza/src/main/java/com/rejasupotaro/MainActivity.java
+++ KinMoza/src/main/java/com/rejasupotaro/MainActivity.java
@@ -3,9 +3,11 @@ package com.rejasupotaro;
 import android.app.Activity;
 
 import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.NoTitle;
 import com.googlecode.androidannotations.annotations.OptionsMenu;
 
 @EActivity(R.layout.activity_main)
+@NoTitle
 @OptionsMenu(R.menu.main)
 public class MainActivity extends Activity {
 }
