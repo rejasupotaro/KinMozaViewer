f2803b3ef639ce88b4a6e9381ba68eaf46f0630b
Kentaro Takiguchi <takiguchi0817@gmail.com>
2013-09-25 20:55:30 +0900

    Initial Commit

---
+++ KinMoza/src/main/java/com/rejasupotaro/MainActivity.java
@@ -0,0 +1,11 @@
+package com.rejasupotaro;
+
+import android.app.Activity;
+
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.OptionsMenu;
+
+@EActivity(R.layout.activity_main)
+@OptionsMenu(R.menu.main)
+public class MainActivity extends Activity {
+}
